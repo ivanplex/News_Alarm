870b14d36ff9de62563aa2f07a5baf97c4998539
Ivan <ivan@ivanplex.com>
2016-10-25 12:36:32 +0100

    Working model

---
+++ play_news.py
@@ -0,0 +1,24 @@
+import glob, os
+
+FILE_DIR = "/home/ivan/Downloads/news"
+os.chdir(FILE_DIR)
+
+'''
+Sort and import all news into a list
+'''
+news_list = []
+for news_file in sorted(glob.glob("*.ts")):
+    news_list.append(news_file)
+
+'Remove news older than 3 days'
+while (len(news_list) > 3):
+	oldest_news = news_list.pop(0)
+	os.system("rm "+oldest_news)
+
+lastest_news = news_list[-1]
+
+command = "totem --fullscreen "+FILE_DIR+"/"+lastest_news
+
+os.system(command)
+
+
---
+++ record_news.py
@@ -0,0 +1,40 @@
+import os
+import datetime
+
+EXEC_IPLAYER = './get_iplayer-2.97/get_iplayer'
+
+BBC_NEWS_AT_6_WEEKDAY_PID = 'b07z8429'
+BBC_NEWS_AT_10_WEEKEND_PID = 'b009m51q'
+
+RECORDING_DESTINATION = '/home/ivan/Downloads/news'
+RECORDINGS_SINCE_LAST_X_HOURS = '22'
+
+
+
+'''
+Get date in the format YYYYmmdd
+E.g. 20161025
+'''
+dateToday = datetime.date.today().strftime("%Y%m%d")
+
+'''
+Get day of the week.
+where 0 is Sunday and 6 is Saturday
+'''
+weekday = int(datetime.date.today().strftime("%w"))
+
+
+command_base = EXEC_IPLAYER+' --file-prefix '+dateToday+' --available-since '+RECORDINGS_SINCE_LAST_X_HOURS+' --modes=hlsvhigh --force -o '+RECORDING_DESTINATION
+
+if(weekday in range(1,5)):
+	print 'Recording weekday 6pm evening news.'
+	command = command_base + ' --pid='+BBC_NEWS_AT_6_WEEKDAY_PID
+	print command
+	os.system(command+">"+RECORDING_DESTINATION+"/log/"+dateToday+".log")
+else:
+	print 'Recording weekend 10pm evening news.'
+	command = command_base + ' --pid='+BBC_NEWS_AT_10_WEEKEND_PID
+	print command
+	os.system(command+">"+RECORDING_DESTINATION+"/log/"+dateToday+".log")
+
+

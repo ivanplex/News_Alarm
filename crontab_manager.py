19302fc287a374286581949cdfe3b0a6899e771e
Ivan <ivan@ivanplex.com>
2016-10-25 22:06:57 +0100

    Search and Record

---
+++ crontab_manager.py
@@ -0,0 +1,15 @@
+import os
+
+'''
+Add crontab job
+
+cron: full cron command
+     E.g.: 00 09 * * 1-5 echo hello
+'''
+def add_crontab(cron):
+    os.system("crontab -l > mycron.temp")
+    os.system("echo "+cron+" >> mycron.temp")
+    os.system("crontab mycron.temp")
+    os.system("rm mycron.temp")
+    return True
+
---
+++ time_reader.py
@@ -0,0 +1,11 @@
+
+'''
+Given a time in string E.g 0600 for 6:00am
+return list
+
+    list[0] - hour
+    list[1] - minute
+'''
+def simple_string_to_time(s_time):
+    return s_time.split(":")
+
